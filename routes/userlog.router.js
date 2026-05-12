a5805b4b7d08a12194d51f81b1479b52f444815a
youssefelsheikh24 <youssefelsheikh24@gmail.com>
2026-05-12 04:26:10 +0300

    fix userlog routes order

--- routes/userlog.router.js
+++ routes/userlog.router.js
@@ -4,13 +4,15 @@ const router = express.Router();
 const verifyUser = require("../Utills/verifyUser");
 const { upload } = require("../Utills/uploadCloudinary");
 
-router.post('/signup',upload.fields([
-    { name: "profile_picture", maxCount: 1 },
-    { name: "national_id", maxCount: 2 },
-  ]),userlogcontroller.createuserlog);
-router.post('/login',userlogcontroller.loginUser);
-router.get('/:id',userlogcontroller.finduserlogbyid);
+router.post('/signup', upload.fields([
+  { name: "profile_picture", maxCount: 1 },
+  { name: "national_id", maxCount: 2 },
+]), userlogcontroller.createuserlog);
+
+router.post('/login', userlogcontroller.loginUser);
 router.post('/forgotpassword', userlogcontroller.forgotPassword);
 router.patch('/resetpassword/:token', userlogcontroller.resetPassword);
-router.patch('/updatepassword',verifyUser, userlogcontroller.updatePassword);
+router.patch('/updatepassword', verifyUser, userlogcontroller.updatePassword);
+
+router.get('/:id', userlogcontroller.finduserlogbyid);
 module.exports = router;
